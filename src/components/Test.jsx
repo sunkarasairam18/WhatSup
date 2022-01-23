2e317269d5ebd114d1b87f2b8ee80039a58526ba
sunkarasairam18 <sunkarasairam@icloud.com>
2022-01-23 13:10:40 +0530

    23 Jan 2021

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,10 +1,20 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
+import { LinkPreview } from "@dhaiwat10/react-link-preview";
 
 const Test = () => {
+  return (
+    <div>
+      <LinkPreview
+        url="https://youtu.be/SkENAjfVoNI"
+        width="350px"
+        textAlign="left"
+        descriptionLength={40}
+        imageHeight={200}
+        secondaryTextColor="black"
+        backgroundColor="lightgray"
+      />
+    </div>
+  );
+};
 
-    return ( 
-        
-    );
-}
- 
 export default Test;
